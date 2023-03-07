d7562d0800d8f03ed032459d36e3ae003e322491
balbachl <lisajbalbach@gmail.com>
2023-03-07 17:20:15 -0500

    Add project files.

---
+++ Data/CustomerContext.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+using testMVCapp.Models;
+
+namespace testMVCapp.Data
+{
+    public class CustomerContext:DbContext
+    {
+            public CustomerContext(DbContextOptions<CustomerContext> options)
+                : base(options)
+            {
+            }
+            public DbSet<Customer> Customers { get; set; }
+
+    }
+}
---
+++ Models/Customer.cs
@@ -0,0 +1,30 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.Xml.Linq;
+
+namespace testMVCapp.Models
+{
+    public class Customer
+    {
+        [Key]
+        public int ID { get; set; }
+
+        [Required(ErrorMessage = "First name is required")]
+        public string FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required")]
+        public string LastName { get; set; }
+
+        public string Address { get; set; }
+        public string City { get; set; }
+        public string State { get; set; }
+        [Display(Name = "Zipcode")]
+        public string PostalCode { get; set; }
+        public string Country { get; set; }
+        public string Phone { get; set; }
+
+        [Required(ErrorMessage = "Email is required")]
+        [RegularExpression(@"[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,4}", ErrorMessage = "Incorrect Email Format")]
+        public string Email { get; set; }
+
+    }
+}
